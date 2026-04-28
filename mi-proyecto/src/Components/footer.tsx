b81fc513c2bcf27a0e20562f60e06a7ae58cd027
CarrizoHamThiago <hamthiago08@gmail.com>
2026-04-28 11:01:13 -0300

    footer 2025 paasaa a 2026

--- mi-proyecto/src/Components/footer.tsx
+++ mi-proyecto/src/Components/footer.tsx
@@ -3,7 +3,7 @@ function Footer(){
         <div className="bg-blue-900">
               <p className="text-1xl text-center text-white"><br />
       <h1 className="text-4xl">Contacto</h1>Correo personal: Hamthiago08@gmail.com || Correo institucional: Carrizo.Ham@tecnica7.edu.ar 
-      <br /><br />© 2025 Carrizo Ham Thiago. Todos los derechos reservados.</p>
+      <br /><br />© 2026 Carrizo Ham Thiago. Todos los derechos reservados.</p>
       </div>
     )
 }
